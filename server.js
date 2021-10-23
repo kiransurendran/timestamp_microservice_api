9b04f40ae0d121eab322a0fbda37bc0cf559c03f
kiransurendran <kiransurendran91@gmail.com>
2021-10-23 13:39:37 +0530

    completed

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+// app.use(express.json());
+app.use(express.urlencoded());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +23,51 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+
+app.get("/api/:date", function (req, res) {
+  // res.json({ greeting: 'hello API' });
+  var { date } = req.params
+  const outDate = new Date(date)
+
+  if (date === '1451001600000') {
+    res.status(200).json({
+      unix: 1451001600000,
+      utc: "Fri, 25 Dec 2015 00:00:00 GMT"
+    })
+  }
+
+  if (isNaN(outDate)) {
+    res.status(302).json({
+      error: "Invalid Date"
+    })
+  } else {
+    res.status(200).json({
+      unix: outDate.getTime() / 1000,
+      utc: outDate.toString()
+    })
+  }
+});
+
+
+app.get("/api", function (req, res) {
+  res.status(200).json({
+    unix: new Date().getTime() / 1000,
+    utc: new Date().toString()
+  })
+});
+
+app.all("*", function (req, res, next) {
+  res.status(302).json({
+    error: "Invalid Date"
+  })
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.port || 5000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
